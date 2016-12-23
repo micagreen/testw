b68330fb2a318c49e1214c31560f698aa209fe24
micagreen <camicablin@gmail.com>
2016-12-23 17:48:53 +0100

    added n

--- wp-content/themes/camille/header.php
+++ wp-content/themes/camille/header.php
@@ -10,7 +10,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- FONT  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
   <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,600' rel='stylesheet' type='text/css'>
-  <!-- Latest compiled and miified CSS -->
+  <!-- Latest compiled and minified CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   <!-- bxSlider CSS-->
   <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory');?>/css/jquery.bxslider.css">
